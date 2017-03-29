67f6866bb2505fd88376c9d4d48cc537efe985dd
smplyamzng <kharmon9452@gmail.com>
2017-03-29 11:49:43 -0400

    structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :user_tickers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def tickers
+  	self.user_tickers.order('created_at DESC')
+  end
 end
